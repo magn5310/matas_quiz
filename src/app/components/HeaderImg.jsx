cb4372d6dd972c7f6331608df80465eaaf171ae7
Valentin <Chris_valentin@hotmail.com>
2023-11-15 11:16:45 +0100

    header + folddownbtn

--- src/app/components/HeaderImg.jsx
+++ src/app/components/HeaderImg.jsx
@@ -1,5 +1,22 @@
 import styles from "./HeaderImg.module.css";
 
+import FoldDownBtn from "./FoldDownBtn";
+
 export default function HeaderImg() {
-  return <header></header>;
+  return (
+    <header
+      className={styles.headerImg}
+      style={{
+        backgroundImage: `url(/pics/model.jpg)`,
+        backgroundPosition: "center",
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    >
+      <FoldDownBtn />
+      <h2 className="text-3xl text-center text-white">
+        Find de bedste <br /> hårprodukter til dit hår
+      </h2>
+    </header>
+  );
 }
