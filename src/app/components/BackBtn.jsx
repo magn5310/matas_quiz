c80aa91a0198778bedf0731d0b2f751df1eac36e
magn5310 <magn5310@stud.kea.dk>
2023-11-15 14:05:13 +0100

    mine components

--- src/app/components/BackBtn.jsx
+++ src/app/components/BackBtn.jsx
@@ -1,3 +1,11 @@
 export default function BackBtn() {
-  return <div></div>;
+  return (
+    <div>
+      <button className="border border-[#FF4F6B] w-12 h-10  rounded grid justify-center items-center">
+        <svg width="8" height="12" viewBox="0 0 8 12" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M7 1L2 6.14286L6.72222 11" stroke="#FF4F6B" stroke-width="2" stroke-linecap="round" />
+        </svg>
+      </button>
+    </div>
+  );
 }
