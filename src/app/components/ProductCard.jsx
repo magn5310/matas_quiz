5083e5fe4ad84fd96db02e042b3bbb00e47b16ee
Valentin <Chris_valentin@hotmail.com>
2023-11-15 11:50:41 +0100

    productcard

--- src/app/components/ProductCard.jsx
+++ src/app/components/ProductCard.jsx
@@ -1,3 +1,31 @@
+import styles from "./ProductCard.module.css";
+import { AiFillStar } from "react-icons/ai";
+import { AiFillHeart } from "react-icons/ai";
+
 export default function ProductCard() {
-  return <div></div>;
+  return (
+    <article className={styles.productCard}>
+      <p className="bg-slate-200 p-1 rounded-lg self-start">Værdi 880,-</p>
+      <img className={styles.productImage} src="/pics/product.jpg" alt="product" />
+      <div className="flex items-center">
+        <AiFillStar />
+        <AiFillStar />
+        <AiFillStar />
+        <AiFillStar />
+        <AiFillStar />
+        <p>(30)</p>
+      </div>
+      <div>
+        <p className="font-bold">Njord</p>
+        <p>Beard Starter Kit</p>
+      </div>
+      <p className="font-bold">349,95 kr.</p>
+      <div className="flex gap-2">
+        <button className="bg-slate-500 font-bold text-white grow rounded-lg p-4">Læg i kurv</button>
+        <button className="bg-slate-200 rounded-lg p-4">
+          <AiFillHeart />
+        </button>
+      </div>
+    </article>
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,10 @@
 import Image from "next/image";
+import ProductCard from "./components/ProductCard";
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <ProductCard />
+    </main>
+  );
 }
