ac8f993708e12f27b2d5da66c7e4be6f6a768bde
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-11-16 10:20:17 +0100

    .

--- src/app/components/ProgressBar.jsx
+++ src/app/components/ProgressBar.jsx
@@ -1,3 +1,15 @@
+import styles from "./ProgessBar.module.css";
+
 export default function ProgressBar() {
-  return <div></div>;
+  return (
+    <div className="px-2">
+      <div className="flex justify-between">
+        <p className="text-xs font-bold text-red-400">31%</p>
+        <p className="text-xs font-bold text-red-400">209 produkter fundet</p>
+      </div>
+      <div className="bg-red-300 w-full h-3 rounded overflow-hidden mt-1">
+        <div className={styles.bar}></div>
+      </div>
+    </div>
+  );
 }
