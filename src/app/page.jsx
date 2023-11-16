7b975afd620af9320e1cfbdb7cb0e60612d60e58
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-11-16 10:27:50 +0100

    ændret

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,9 @@
 import Image from "next/image";
-import Quiz from "./components/Quiz";
 
 
 export default function Home() {
-  return <main className="bg-slate-100">
-<Quiz/>
+  return <main>
+
   
   
   </main>;
