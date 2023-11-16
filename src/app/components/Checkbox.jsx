ad50e97e21970e19f078fe536301d404290f4baf
Valentin <Chris_valentin@hotmail.com>
2023-11-16 11:33:15 +0100

    use-clinet på buttons

--- src/app/components/Checkbox.jsx
+++ src/app/components/Checkbox.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+"use client";
+import { useState } from "react";
 
 export default function Checkbox(props) {
   const [isChecked, setIsChecked] = useState(false);
@@ -8,21 +9,9 @@ export default function Checkbox(props) {
   };
 
   return (
-    <label
-      className={`flex w-full justify-between border border-solid py-1 font px-2 rounded border-rose-500 cursor-pointer only-after:bg-[#ff4f6b] hover:bg-red-50 ${
-        isChecked ? 'bg-rose-500 text-white  hover:bg-red-300' : 'text-rose-500'
-      }`}
-      htmlFor={props.label}
-
-    >
+    <label className={`flex w-full justify-between border border-solid py-1 font px-2 rounded border-rose-500 cursor-pointer only-after:bg-[#ff4f6b] hover:bg-red-50 ${isChecked ? "bg-rose-500 text-white  hover:bg-red-300" : "text-rose-500"}`} htmlFor={props.label}>
       {props.label}
-      <input
-        className="rounded-full  accent-rose-500 border border-solid border-white outline-rose-500"
-        id={props.label}
-        type="checkbox"
-        checked={isChecked}
-        onChange={toggleCheckbox}
-      />
+      <input className="rounded-full  accent-rose-500 border border-solid border-white outline-rose-500" id={props.label} type="checkbox" checked={isChecked} onChange={toggleCheckbox} />
     </label>
   );
 }
--- src/app/components/NextBtn.jsx
+++ src/app/components/NextBtn.jsx
@@ -1,7 +1,5 @@
+"use client";
+
 export default function NextBtn() {
-  return (
-    
-      <button className="bg-rose-500 text-white w-full h-10 font-semibold  rounded flex justify-center items-center">Næste</button>
-   
-  );
+  return <button className="bg-rose-500 text-white w-full h-10 font-semibold  rounded flex justify-center items-center">Næste</button>;
 }
