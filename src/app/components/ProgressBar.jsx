84e645301187a926b57b5fe8dcc2d2a3560b7b2e
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-11-17 11:46:08 +0100

    til christ

--- src/app/components/ProgressBar.jsx
+++ src/app/components/ProgressBar.jsx
@@ -1,16 +1,25 @@
 import styles from "./ProgessBar.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
 export default function ProgressBar() {
+  const myArray = ["0", "1/4", "2/4", "3/4", "full"];
+  const [fraction, setFraction] = useState(myArray[0]);
+
+  function changeFraction() {
+    setFraction((myArray[0]) => myArray[]);
+  }
+
   return (
     <div className="px-2 w-full">
+      <button onClick={changeFraction}>hey</button>
       <div className="flex justify-between">
         <p className="text-xs font-bold text-rose-500">31%</p>
         <p className="text-xs font-bold text-rose-500">209 produkter fundet</p>
       </div>
       <div className="bg-rose-200 w-full h-3 rounded overflow-hidden mt-1">
         {/* <div className={styles.bar}></div> */}
-        <div className="h-full w-40">
+        <div className={`h-full w-${fraction}`}>
           <Image className="h-full w-full object-cover" src="/pics/bar-bg.svg" width={400} height={10} alt="Quiz Progess" />
         </div>
       </div>
