f673192a2dc4ca5a501664c75d9e7b50886eb6ef
Ballademager <118718923+Ballademager@users.noreply.github.com>
2023-11-22 13:37:51 +0100

    hover color checkbox

--- src/app/components/Checkbox.jsx
+++ src/app/components/Checkbox.jsx
@@ -2,39 +2,27 @@
 import { stringify } from "postcss";
 import { useState } from "react";
 
-
-
 export default function Checkbox(props) {
-
-
   const [isChecked, setIsChecked] = useState(props.checked);
 
   const toggleCheckbox = () => {
     setIsChecked(!isChecked);
 
-    
+    const number = (props.questionIndex + 1).toString();
+    const qIndex = "q" + number;
+    const aIndex = "a" + number;
 
-const number = (props.questionIndex+1).toString();
-const qIndex = "q" + number;
-const aIndex = "a" + number;
+    props.getAnswerAndSend(qIndex, aIndex, props.label);
 
-props.getAnswerAndSend(qIndex, aIndex, props.label)
-
-console.log("mit index", qIndex)
-
-
-    
+    console.log("mit index", qIndex);
   };
 
   return (
     <label
-      className={`flex justify-between border border-solid py-1 font px-2 max-h-[34px] rounded border-rose-500 cursor-pointer only-after:bg-[#ff4f6b] hover:bg-red-50 ${isChecked ? "bg-rose-500 text-white  hover:bg-red-300" : "text-rose-500"}`}
+      className={`flex justify-between border border-solid py-1 font px-2 max-h-[34px] rounded border-rose-500 cursor-pointer only-after:bg-[#ff4f6b] hover:bg-red-50 ${isChecked ? "bg-rose-500 text-white  hover:bg-rose-600" : "text-rose-500"}`}
       htmlFor={props.label}>
       {props.label}
       <input className="rounded-full  accent-rose-500 border border-solid border-white outline-rose-500" id={props.label} type="checkbox" checked={isChecked} onChange={toggleCheckbox} />
-
-      
-
     </label>
   );
 }
