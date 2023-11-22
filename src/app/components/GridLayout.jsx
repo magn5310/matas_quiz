0bbbbb944364d1977c52634fba9b0622a6f3ca6e
juli81n7 <112468707+juli81n7@users.noreply.github.com>
2023-11-22 18:52:43 +0100

    Lavet loginCard

--- src/app/components/GridLayout.jsx
+++ src/app/components/GridLayout.jsx
@@ -2,6 +2,7 @@
 import ProductCard from "./ProductCard";
 import Quiz from "./Quiz";
 import QuizCard from "./QuizCard";
+import LoginCard from "./LoginCard";
 import { useState, useEffect } from "react";
 export default function GridLayout({ data, data2 }) {
   console.log("Det her er min NYE data.", data);
@@ -31,8 +32,8 @@ export default function GridLayout({ data, data2 }) {
     setVisteProdukter(nyeProdukter);
   };
 
-  async function postAnswer(answer) {
-    console.log(answer);
+  async function postAnswer() {
+    console.log("JEG POSTER DET HER NU",answer);
     let headersList = {
       Accept: "*/*",
       apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdocmZpanV2YXVpcnB3b2lydGNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODMzMDI4ODcsImV4cCI6MTk5ODg3ODg4N30.OYkOttfTxQpFWSIFoRK-f0qW2S8j7U3tg6kAC3BLcnY",
@@ -59,7 +60,13 @@ export default function GridLayout({ data, data2 }) {
   return (
     <article className="relative">
       <div className="grid grid-cols-mainGrid gap-4 p-32 items-stretch relative">
+      {visteProdukter.length < 50 ? (
+        <LoginCard postAnswer={postAnswer} setAnswer={setAnswer} answer={answer} />
+      ) : (
         <QuizCard toggleQuiz={toggleHandler} />
+      )}
+      
+    
 
         {visteProdukter.map((item) => (
           <ProductCard name={item.name} image={item.image} brand={item.brand} id={item.id} price={item.price} key={item.id} />
--- src/app/components/LoginCard.jsx
+++ src/app/components/LoginCard.jsx
@@ -1,14 +1,63 @@
-"use client";
+import React, { useState } from 'react';
 
-export default function LoginCard({ toggleQuiz }) {
+
+
+export default function LoginCard(props) {
+  const [name, setName]= useState();
+  const [loggedIn, setLoggedIn]= useState(false);
+
+
+  const handleUsernameChange = (event) => {
+    const newObject = { ...props.answer, name: event.target.value };
+setName(event.target.value)
+    props.setAnswer(newObject);
+    console.log("dette er det jeg har gang i nu", props.answer);
+   };
+
+  
+   const handleSubmit = (event) => {
+    
+    event.preventDefault(); // Prevents the default form submission behavior
+    setLoggedIn(true)
+    props.postAnswer()
+
+    
+  };
+
   return (
-    <article className="flex flex-col p-4 bg-white max-w-xs justify-center items-center rounded-lg overflow-hidden cursor-pointer">
-    <h3>Login</h3>
-    <p>for at gemme dine resultater</p>
-    <label htmlFor="">
-    <input type="text" />
-    <button></button>
-    </label>
+    <article className="flex flex-col p-4 bg-white  max-w-xs justify-center items-center rounded-lg overflow-hidden ">
+    
+    {loggedIn ? (
+      <>
+      <h3 className="font-bold">Velkommen tilbage {name}</h3>
+      <p>Se dine resultater her</p>
+      <button className="cursor-pointer text-white mt-2 bg-rose-500 px-4 py-2 rounded-sm">
+      Din side
+    </button>
+      </>
+    ) : (
+      <form className='flex flex-col justify-center items-center' onSubmit={handleSubmit}>
+   
+      <h3 className="font-bold">Login</h3>
+      <p className="text-center font-light text-slate-600 text-sm">for at gemme dine resultater</p>
+      <label className="mt-5 text-center" htmlFor="username">
+        Brugernavn
+        <input
+          className=" flex flex-col bg-slate-200 mt-2 py-1 rounded px-1"
+          id="username"
+          name="username"
+          type="text"
+          onChange={handleUsernameChange}
+        />
+      </label>
+      <button type="submit" className="cursor-pointer text-white mt-2 bg-rose-500 px-4 py-2 rounded-sm">
+        Login
+      </button>
+      </form>
+      
+    )}
     </article>
+    
+   
   );
 }
