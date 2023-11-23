b7984e555cbfd358f4ed1ba7177873738043d6af
Valentin <Chris_valentin@hotmail.com>
2023-11-23 19:25:57 +0100

    wording

--- src/app/components/SecondaryBtn.jsx
+++ src/app/components/SecondaryBtn.jsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 export default function SecondaryBtn() {
   return (
     <button className="text-rose-500 lg:mt-auto justify-items-center col-span-full sticky bottom-0 self-center pt-4">
-      <Link target="_blank" href={"https://www.matas.dk/det-sker"}>
-        <h3 className="text-xl font-semibold">Er du i tvivl?</h3>
+      <Link target="_blank" href={"https://www.matas.dk/ekspert-team"}>
+        <h3 className="text-xl font-semibold">Stadig i tvivl?</h3>
         <p className="text-xs">Book en tid hos en ekspert</p>
       </Link>
     </button>
